2207fbe30bccec556fc4d443a0da3d53d5ffe3a4
maoshuo1754 <1664480444@qq.com>
2025-05-09 19:18:53 +0800

    pinned memery 在读文件下的bug修改

--- SharedQueue.h
+++ SharedQueue.h
@@ -4,6 +4,7 @@
 #include <semaphore.h>
 #include <cuda_runtime.h>
 #include <iostream>
+#include "Config.h"
 #define SHM_KEY 0x1234
 #define QUEUE_SIZE 4                // 队列大小
 #define BLOCK_SIZE (256*1024*1024)  // 256MB
@@ -24,14 +25,19 @@ struct SharedQueue {
         cudaError_t err;
 
         index_buffer = new unsigned char[QUEUE_SIZE * INDEX_SIZE];
-        // buffer = new unsigned char[QUEUE_SIZE * BLOCK_SIZE];
-        err = cudaHostAlloc(&buffer, QUEUE_SIZE * BLOCK_SIZE, cudaHostAllocDefault);
-        if (err != cudaSuccess) {
-            std::cerr << "Failed to allocate pinned memory for buffer: "
-                      << cudaGetErrorString(err) << std::endl;
-            cudaFreeHost(index_buffer); // 清理已分配的内存
-            throw std::runtime_error("cudaHostAlloc failed");
+        if(dataSource_type == 0) {
+            buffer = new unsigned char[QUEUE_SIZE * BLOCK_SIZE];
         }
+        else {
+            err = cudaHostAlloc(&buffer, QUEUE_SIZE * BLOCK_SIZE, cudaHostAllocDefault);
+            if (err != cudaSuccess) {
+                std::cerr << "Failed to allocate pinned memory for buffer: "
+                          << cudaGetErrorString(err) << std::endl;
+                cudaFreeHost(index_buffer); // 清理已分配的内存
+                throw std::runtime_error("cudaHostAlloc failed");
+            }
+        }
+
         sem_init(&this->mutex, 1, 1);
         sem_init(&this->slots_available, 1, QUEUE_SIZE);
         sem_init(&this->items_available, 1, 0);
@@ -41,7 +47,13 @@ struct SharedQueue {
 
     ~SharedQueue() {
         delete[] index_buffer;
-        cudaFreeHost(buffer);
+        if(dataSource_type == 0) {
+            delete[] buffer;
+        }
+        else {
+            cudaFreeHost(buffer);
+        }
+
         // 销毁信号量
         sem_destroy(&this->mutex);
         sem_destroy(&this->slots_available);
